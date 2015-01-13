e445a011374b7766f0501d59b0ad16d6ead224cc
Leonardo Comelli <leonardo.comelli@gmail.com>
2015-01-13 12:35:32 +0000

    fixed the password field name

--- gh_keys.py
+++ gh_keys.py
@@ -22,7 +22,7 @@ options:
     description: Github username.
     required: true
 
-  passwd:
+  password:
     description: Github password. If 2FA is enabled for your account, you should generate a new personal access token. Required for get_key, add_key and remove_key 
     required: false 
 
@@ -48,16 +48,16 @@ EXAMPLES = '''
 - gh_keys: action=list_keys user=leocomelli
 
 # Lists all informations about all keys of authenticated user
-- gh_keys: action=list_keys user=leocomelli passwd=secret
+- gh_keys: action=list_keys user=leocomelli password=secret
 
 # Gets a single public key (authetication required)
-- gh_keys: action=get_key user=leocomelli passwd=secret key_id=8767854
+- gh_keys: action=get_key user=leocomelli password=secret key_id=8767854
 
 # Adds a new public key (authentication required)
-- gh_keys: action=add_key user=leocomelli passwd=secret title=my_new_key key=/home/leocomelli/.ssh/id_rsa.pub
+- gh_keys: action=add_key user=leocomelli password=secret title=my_new_key key=/home/leocomelli/.ssh/id_rsa.pub
 
 # Removes an existing ssh key
-- gh_keys: action=remove_key user=leocomelli passwd=secret key_id=8767854
+- gh_keys: action=remove_key user=leocomelli password=secret key_id=8767854
 '''
 
 GH_API_URL = "https://api.github.com/%s"
@@ -68,7 +68,7 @@ class GHKeys(object):
     self.module = module
     self.action = module.params['action']
     self.user   = module.params['user']
-    self.passwd = module.params['passwd']
+    self.password = module.params['password']
     self.title  = module.params['title']
     self.key    = module.params['key']
     self.key_id = module.params['key_id']
@@ -82,43 +82,43 @@ class GHKeys(object):
     }[self.action]()
 
   def list_keys(self):
-    if self.passwd is None:
+    if self.password is None:
       url = GH_API_URL % "users/%s/keys" % self.user
       response, info = fetch_url(self.module, url)
     else:
       url = GH_API_URL % "user/keys"
-      headers = self.get_auth_header(self.user, self.passwd)
+      headers = self.get_auth_header(self.user, self.password)
       response, info = fetch_url(self.module, url, headers=headers)
     
     return response.read(), info
  
   def get_key(self):
-    self.validate_fileds('get_key', ['key_id', 'passwd'])    
+    self.validate_fileds('get_key', ['key_id', 'password'])    
 
     url = GH_API_URL %  "user/keys/%s" % self.key_id
-    headers = self.get_auth_header(self.user, self.passwd)
+    headers = self.get_auth_header(self.user, self.password)
     response, info = fetch_url(self.module, url, headers=headers)
 
     return response.read(), info
 
   def add_key(self):
-    self.validate_fileds('add_key', ['title', 'key', 'passwd'])
+    self.validate_fileds('add_key', ['title', 'key', 'password'])
 
     with open(self.key, 'r') as content_file:
       content = content_file.read()
 
     url = GH_API_URL %  "user/keys"
-    headers = self.get_auth_header(self.user, self.passwd)
+    headers = self.get_auth_header(self.user, self.password)
     data = json.dumps({ 'title' : self.title, 'key' : content })
     response, info = fetch_url(self.module, url, headers=headers, data=data)
 
     return response.read(), info
 
   def remove_key(self):
-    self.validate_fileds('remove_key', ['key_id', 'passwd'])
+    self.validate_fileds('remove_key', ['key_id', 'password'])
 
     url = GH_API_URL %  "user/keys/%s" % self.key_id
-    headers = self.get_auth_header(self.user, self.passwd)
+    headers = self.get_auth_header(self.user, self.password)
     response, info = fetch_url(self.module, url, headers=headers, method='DELETE')
 
     return response.read(), info
@@ -142,12 +142,12 @@ def convert_bool_to_str(value):
 def main():
   module = AnsibleModule(
     argument_spec = dict(
-      passwd = dict(),
-      title  = dict(),
-      key    = dict(),
-      key_id = dict(),
-      user   = dict(required=True),
-      action = dict(required=True, choices=['list_keys', 'get_key', 'add_key', 'remove_key']),
+      password = dict(),
+      title    = dict(),
+      key      = dict(),
+      key_id   = dict(),
+      user     = dict(required=True),
+      action   = dict(required=True, choices=['list_keys', 'get_key', 'add_key', 'remove_key']),
     )
   )  
 
